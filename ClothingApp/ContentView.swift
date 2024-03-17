26e5870c2424b29842d769a4a3e86fe18b264264
Amasha997 <amashanuwanthi777@gmail.com>
2024-03-17 14:47:31 +0530

    NavigationViewAdded

--- ClothingApp/ContentView.swift
+++ ClothingApp/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
         ZStack {
             
             NavigationView()
-            
+                
             SplashScreenView(endAnimation: $endAnimation)
         }
     }
--- ClothingApp/NavigationView.swift
+++ ClothingApp/NavigationView.swift
@@ -8,8 +8,92 @@
 import SwiftUI
 
 struct NavigationView: View {
+    
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @Namespace var animation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $currentTab) {
+            Text("Home View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Home)
+            
+            Text("Search View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Search)
+            
+            Text("Notification View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Notifications)
+            
+            Text("Cart View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Cart)
+            
+            Text("Profile View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Profile)
+        }
+        .overlay(
+            HStack(spacing:0){
+                ForEach(Tab.allCases, id: \.rawValue){
+                    tab in TabButton(tab: tab)
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("Color 3"))
+            }
+            ,
+            alignment: .bottom
+        )
+        .ignoresSafeArea(.all, edges: .bottom)
+    }
+    
+    func TabButton(tab: Tab) -> some View {
+        GeometryReader{
+            proxy in
+            Button(action: {
+                withAnimation(.spring()){
+                    currentTab = tab
+                }
+            },label: {
+                VStack(spacing: 0){
+                    Image(systemName: currentTab == tab ? tab.rawValue + ".fill": tab.rawValue)
+                        .resizable()
+                        .foregroundColor(Color("Color 1"))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .background(
+                            ZStack{
+                                if currentTab == tab {
+                                    MaterialEffect(style: .light)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                        .matchedGeometryEffect(id: "Tab", in: animation)
+                                    
+                                    Text(tab.TabName)
+                                        .foregroundColor(Color("Color 1"))
+                                        .font(.footnote)
+                                        .padding(.top, 50)
+                                }
+                            }
+                        )
+                        .contentShape(Rectangle())
+                        .offset(y: currentTab == tab ? -15 : 0)
+                }
+            })
+        }
+        .frame(height: 25)
     }
 }
 
@@ -20,4 +104,35 @@ struct NavigationView: View {
 enum Tab: String, CaseIterable{
     case Home = "house"
     case Search = "magnifyingglass.circle"
+    case Notifications = "bell"
+    case Cart = "bag"
+    case Profile = "person"
+    
+    var TabName: String{
+        switch self{
+        case .Home:
+            return "Home"
+        case .Search:
+            return "Search"
+        case .Notifications:
+            return "Notifications"
+        case .Cart:
+            return "Cart"
+        case .Profile:
+            return "Profile"
+        }
+    }
+}
+
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style:style))
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
 }
